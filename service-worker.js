f6e00a252b10e644f8087189ef6fd8a0728b0728
Rendall <rendall@gmail.com>
2017-12-18 10:12:35 +0200

    ui update

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,44 @@
-var CACHE_NAME="bm-mapp-app",urlsToCache=["/","/static/js/main.9947668f.js","/js/search-engine-worker.js","/css/style.css","/favicon.ico","/js/fuse.min.js","/js/Rx.min.js","/js/SearchEngine.js","/service-worker.js"];self.addEventListener("install",function(e){e.waitUntil(caches.open(CACHE_NAME).then(function(e){return e.addAll(urlsToCache)}))}),self.addEventListener("fetch",function(e){e.respondWith(caches.match(e.request).then(function(n){if(n)return n;var t=e.request.clone();return fetch(t).then(function(n){if(!n||200!==n.status||"basic"!==n.type)return n;var t=n.clone();return caches.open(CACHE_NAME).then(function(n){n.put(e.request,t)}),n})}))}),self.addEventListener("activate",function(e){var n=[CACHE_NAME];e.waitUntil(caches.keys().then(function(e){return Promise.all(e.map(function(e){return-1===n.indexOf(e)?caches["delete"](e):void 0}))}))});
-console.info(`'BMApp service-worker installed'`);
+var CACHE_NAME = 'bm-mapp-app';
+var urlsToCache = [
+    '/',
+    '/static/js/main.092e6c7e.js',
+    '/js/search-engine-worker.js',
+    '/css/style.css',
+    '/favicon.ico',
+    '/js/fuse.min.js',
+    '/js/Rx.min.js',
+    '/js/SearchEngine.js',
+    '/service-worker.js'
+];
+self.addEventListener('install', function (event) {
+    console.log("service worker install event", event);
+    // Perform install steps
+    event.waitUntil(caches.open(CACHE_NAME)
+        .then(function (cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+    }));
+});
+self.addEventListener('fetch', function (event) {
+    event.respondWith(caches.match(event.request).then(function (resp) {
+        return resp || fetch(event.request).then(function (response) {
+            caches.open(CACHE_NAME).then(function (cache) {
+                cache.put(event.request, response.clone());
+            });
+            return response;
+        });
+    }).catch(function () {
+        return caches.match('/');
+    }));
+});
+self.addEventListener('activate', function (event) {
+    var cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(caches.keys().then(function (cacheNames) {
+        return Promise.all(cacheNames.map(function (cacheName) {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+                return caches.delete(cacheName);
+            }
+        }));
+    }));
+});
+console.info("'BMApp service-worker installed'");
